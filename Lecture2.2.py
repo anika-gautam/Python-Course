e4bcb9840d94b849a8770dcc2372f7af16aed25a
anika-gautam <anika.vgautam@gmail.com>
2025-08-04 20:03:30 -0700

    Add files via upload

---
+++ Lecture2.2.py
@@ -0,0 +1,12 @@
+a=input("Write a number,")
+b=input("Write another number,")
+c=input("Write one more number,")
+if (a>=c and a>=b):
+    print(a ,"is the greatest number ")
+elif (b>=a and b>=c ):
+    print(b,"is the greatest number")
+else:
+    print(c, "is the greatest number")    
+
+
+
---
+++ Lecture2.py
@@ -0,0 +1,4 @@
+name=input("Enter your name:")
+print("The length of your name is",len(name))
+#str="Hi, $Iam the $ symbol $99.99"
+#print("There are ", str.count("$"), "$ symbols in your sentence.")
